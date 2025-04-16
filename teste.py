5905d2569fcf5bfc113208f613fc503c7f2e4b91
brunocmt85 <brunocmt@gmail.com>
2025-04-15 23:47:12 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,12 +1,11 @@
 import time
 
-# Lista que nunca é liberada da memória
+
 vazamento = []
 
 def vazamento_de_memoria():
     while True:
-        # Cria muitos dados e armazena permanentemente
-        vazamento.append("A" * 10**6)  # 1 MB de dados
+        vazamento.append("A" * 10**6)  
         print(f"Objetos armazenados: {len(vazamento)}")
         time.sleep(0.5)
 
