25c97c3c1735acdaab08af28aacebd7cafc87f56
brunocmt85 <brunocmt@gmail.com>
2025-04-24 15:41:57 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,10 +1,10 @@
 import time
-vazamento[]   
+vazamento2[]   
  
 def vazamento_de_memoria():
     while True:
-        vazamento.append("A" * 10**6)  
-        print(f"Objetos armazenados: {len(vazamento)}")
+        vazamento2.append("A" * 10**6)  
+        print(f"Objetos armazenados: {len(vazamento2)}")
         time.sleep(0.5)
 
 if __name__ == "__main__":
