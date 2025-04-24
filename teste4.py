9a84237f808124ad7cdd271ed54b52e3c102aac2
brunocmt85 <brunocmt@gmail.com>
2025-04-24 20:56:19 -0300

    Create teste4.py

---
+++ teste4.py
@@ -0,0 +1,12 @@
+import time    
+vazamento[]   
+print("teste4")
+  
+def vazamento_de_memoria():
+    while True:
+        vazamento.append("A" * 10**6)  
+        print(f"Objetos armazenados: {len(vazamento)}")
+        time.sleep(0.5)
+
+if __name__ == "__main__":
+    vazamento_de_memoria()
