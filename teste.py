3be15f40f5b88045466a0736039168e7008f547a
brunocmt85 <brunocmt@gmail.com>
2025-04-26 18:03:38 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,10 +2,10 @@ import time
 
 # Simulação2
 def memory_leak_simulation():
-    leaked_list = []  # Lista que vai crescer indefinidamente
+    leaked_list = []  
     while True:
-        leaked_list.append('A' * 10**6)  # Adiciona 1 MB de dados a cada iteração
-        time.sleep(0.1)  # Pequeno atraso para observar o consumo gradual de memória
+        leaked_list.append('A' * 10**6)  
+        time.sleep(0.1) 
 
 if __name__ == "__main__":
     memory_leak_simulation()
