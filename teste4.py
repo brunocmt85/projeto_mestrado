974a4df034a0d8e3927cfacee430e4461a004c6a
brunocmt85 <brunocmt@gmail.com>
2025-06-12 08:37:07 -0300

    Update teste4.py

--- teste4.py
+++ teste4.py
@@ -1,12 +1,18 @@
-import time    
-vazamento[]   
-print("teste11")
-  
+import time
+
 def vazamento_de_memoria():
-    while True:
-        vazamento.append("A" * 10**6)  
-        print(f"Objetos armazenados: {len(vazamento)}")
-        time.sleep(0.5)
+    vazamento = []
+    try:
+        while True:
+            vazamento.append("A" * 10**6)
+            print(f"Objetos armazenados: {len(vazamento)}")
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("Processo interrompido.")
+    finally:
+        del vazamento
+        print("Memória liberada.")
 
 if __name__ == "__main__":
+    print("39")
     vazamento_de_memoria()
